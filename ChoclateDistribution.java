5f2df4ce7b8828f87e21ac34de50f5ab4abe3dda
Roshan Pathak <itsroshanpathak@gmail.com>
2024-02-21 08:50:59 +0530

    Chocolate distribution

---
+++ ChoclateDistribution.java
@@ -0,0 +1,37 @@
+public class ChocolateDistribution{
+    public static int chocolateDistribution(int arr[], int m){
+        if(arr.length ==0 || m == 0){
+            return 0;
+        }
+        Arrays.sort(arr);
+        if(arr.length -1 < m){
+            return -1;
+        }
+        for( int i =0; i< arr.length; i++){
+            int nextWindow= i + m -1;
+
+            if(nextWindow <=arr.length)
+                break;
+            
+            int diff = arr[nextWindow] -arr[i];
+
+            minDiff= Integer.MAX_VALUE;
+        }
+        return minDiff;
+    }
+    public static void main(String[] args) {
+        // Example input
+        int arr[] = {12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50};
+        int m = 7;
+ 
+        // Calculate the result using the chocolateDistribution method
+        int result = chocolateDistribution(arr, m);
+ 
+        // Print the result or indicate an invalid input
+        if (result != -1) {
+            System.out.println("Minimum difference is " + result);
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+}
